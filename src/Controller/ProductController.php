f774e21c07196b2ddd4745df5c231fa3cd2f12f7
nhinhampro <minhbngch18369@fpt.edu.vn>
2022-08-15 17:02:23 +0700

    minh

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/product')]
 class ProductController extends AbstractController
 {
     #[IsGranted("ROLE_ADMIN")]
-    #[Route('/admin', name: 'admin_product')]
+    #[Route('/admin', name: 'product_index')]
     public function adminProduct(ProductRepository $productRepository){
         $products = $productRepository;
         return $this->render('product/index.html.twig',
@@ -30,7 +31,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/detail/{id}', name: 'productdetail')]
+    #[Route('/detail/{id}', name: 'product_detail')]
     public function bookDetail ($id, ProductRepository $productRepository) {
       $products = $productRepository->find($id);
       if ($products == null) {
@@ -43,5 +44,21 @@ class ProductController extends AbstractController
           ]);
     }
 
-    
+
+
+    // ----------------------------------------------------------------------
+    #[IsGranted("ROLE_CUSTOMER")]
+    #[Route('/search', name:'search_product')]
+    public function searchBook(ProductRepository $productRepository, Request $request){
+      $products = $productRepository->searchBook($request->get('keyword'));
+      if ($products == null){
+          $this->addFlash("Warning", "No product found !");
+      }
+      $session = $request->getSession();
+      $session->set('search', true);
+      return $this->render('product/list.html.twig',
+      [
+          'products' => $products,
+      ]);
+    }
 }
