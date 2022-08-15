3fd3dcc9ae01739604058546b5a948e4fc3da85f
Do Trung Thanh <dotrungthanh2300@gmail.com>
2022-08-16 01:44:17 +0700

    a

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +12,15 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('name', TextType::class,
+            [
+                'label' => 'Product Name',
+                'attr' => [
+                    'minlength' => 4,
+                    'class' = 'dm'
+                ],
+                
+            ])
         ;
     }
 
