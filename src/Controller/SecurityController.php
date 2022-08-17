af31b03ddfea07241a0979106781721dc69ed5fb
Do Trung Thanh <dotrungthanh2300@gmail.com>
2022-08-17 09:33:30 +0700

    a

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,8 +12,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('home');
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('admin_home');
+        } else {
+            return $this->redirectToRoute('customer_home');
         }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -23,7 +25,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
