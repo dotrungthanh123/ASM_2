d91ecb1a3c884d5f41760610d4b240fbbd9da632
NguyenTheManh <manh9403@gmail.com>
2022-08-17 09:33:21 +0700

    manh

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,7 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'admin_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            
-        ]);
+        return $this->render('home/index.html.twig');
     }
 
     #[Route('/home', name: 'customer_home')]
