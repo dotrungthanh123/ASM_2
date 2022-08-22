2ad5a443078cfb9b3fb14956355fa910e0470ae2
nhinhampro <minhbngch18369@fpt.edu.vn>
2022-08-23 01:03:05 +0700

    .

--- src/DataFixtures/PorductFixtures.php
+++ src/DataFixtures/PorductFixtures.php
@@ -11,14 +11,14 @@ class PorductFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $product = new Product;
-        // for ($i = 0; $i < 20; $i++) {
-        //     $product->setName("Product $i");
-        //     $product->setImage("https://th.bing.com/th/id/OIP.boU0VLtfyLfKIbp_1YIWJgHaFj?pid=ImgDet&rs=1");
-        //     $product->setQuantity($i);
-        //     $product->setDescription('sdkjfdsksnjdksf');
-        //     $product->setPrice((float) $i);
-        //     $manager->persist($product);
-        // }
+        for ($i = 0; $i < 20; $i++) {
+            $product->setName("Product $i");
+            $product->setImage("https://th.bing.com/th/id/OIP.boU0VLtfyLfKIbp_1YIWJgHaFj?pid=ImgDet&rs=1");
+            $product->setQuantity($i);
+            $product->setDescription('sdkjfdsksnjdksf');
+            $product->setPrice((float) $i);
+            $manager->persist($product);
+        }
         $manager->flush();
     }
 }
